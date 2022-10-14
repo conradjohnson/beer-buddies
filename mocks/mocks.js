f6082229beca4848e36a9a38ec5c83751276464e
Austin Moore <austin1moore@gmail.com>
2022-10-14 11:07:31 -0500

    working on dashboard tab toggle

--- mocks/mocks.js
+++ mocks/mocks.js
@@ -5,7 +5,21 @@ let clButtonEl = document.querySelector("#clButton")
 let lpButtonEl = document.querySelector("#lpButton")
 let lbButtonEl = document.querySelector("#lbButton")
 
-checklistEl.addEventListener("click", toggleChecklist);
+clButtonEl.addEventListener("click", toggleChecklist);
 function toggleChecklist() {
-    
+    checklistEl.setAttribute("class", "");
+    leaderboardEl.setAttribute("class", "hidden");
+    postsEl.setAttribute("class", "hidden");
+  }
+  lpButtonEl.addEventListener("click", togglePost);
+function togglePost() {
+    checklistEl.setAttribute("class", "hidden");
+    leaderboardEl.setAttribute("class", "hidden");
+    postsEl.setAttribute("class", "");
+  }
+  lbButtonEl.addEventListener("click", toggleLeaderboard);
+function toggleLeaderboard() {
+    checklistEl.setAttribute("class", "hidden");
+    leaderboardEl.setAttribute("class", "");
+    postsEl.setAttribute("class", "hidden");
   }
