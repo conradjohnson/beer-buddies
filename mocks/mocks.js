4a800fc25e1b462e58509493b2974eea36e2aead
Austin Moore <austin1moore@gmail.com>
2022-10-14 10:52:15 -0500

    completed homepage and profile, working on login and dashboard

---
+++ mocks/mocks.js
@@ -0,0 +1,11 @@
+let checklistEl = document.querySelector("#beerChecklist")
+let leaderboardEl = document.querySelector("#beerLeaderboard")
+let postsEl = document.querySelector("#latestPosts")
+let clButtonEl = document.querySelector("#clButton")
+let lpButtonEl = document.querySelector("#lpButton")
+let lbButtonEl = document.querySelector("#lbButton")
+
+checklistEl.addEventListener("click", toggleChecklist);
+function toggleChecklist() {
+    
+  }
