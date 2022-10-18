62951f45e2e82a02ab2541d9b37011542a8da769
conradjohnson <skaterjames@gmail.com>
2022-10-18 10:35:10 -0500

    trying to force heroku db

--- seeds/seed.js
+++ seeds/seed.js
@@ -10,37 +10,37 @@ const beerlistData = require('./beerlistData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  for (const user of userData){
-    await User.create ({
-      ...user
-    }, {
-      individualHooks: true,
-      returning: true
-    })
-  }
-  // const users = await User.bulkCreate(userData, {
-  //   individualHooks: true,
-  //   returning: true,
-  // });
+  // for (const user of userData){
+  //   await User.create ({
+  //     ...user
+  //   }, {
+  //     individualHooks: true,
+  //     returning: true
+  //   })
+  // }
+  // // const users = await User.bulkCreate(userData, {
+  // //   individualHooks: true,
+  // //   returning: true,
+  // // });
   
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: 1,
-    });
-  }
-  const comments = await Comment.bulkCreate(commentData,{
-        individualHooks: true,
-        returning: true,
-      });
-  const beers = await Beer.bulkCreate(beerData,{
-        individualHooks: true,
-        returning: true,
-      });
-  const beerlist = await Beerlist.bulkCreate(beerlistData, {
-        individualHooks: true,
-        returning: true,
-      });
+  // for (const post of postData) {
+  //   await Post.create({
+  //     ...post,
+  //     user_id: 1,
+  //   });
+  // }
+  // const comments = await Comment.bulkCreate(commentData,{
+  //       individualHooks: true,
+  //       returning: true,
+  //     });
+  // const beers = await Beer.bulkCreate(beerData,{
+  //       individualHooks: true,
+  //       returning: true,
+  //     });
+  // const beerlist = await Beerlist.bulkCreate(beerlistData, {
+  //       individualHooks: true,
+  //       returning: true,
+  //     });
 
   process.exit(0);
 };
