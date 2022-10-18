06eb22d16acfa7e625aae9d767be962756275a39
Austin Moore <austin1moore@gmail.com>
2022-10-18 09:29:38 -0500

    working on postjs/merging

--- public/js/post.js
+++ public/js/post.js
@@ -1,25 +1,30 @@
+
 const addCommentHandler = async (event) => {
     event.preventDefault();
-  
-    const body = await nl2br(document.querySelector('#comment-body').value.trim());
-    const post_id = document.querySelector('#post-id').value;
-    
-    if (body && post_id) {
+
+    // const body = await nl2br(document.querySelector('#comment-body').value.trim());
+    // const post_id = document.querySelector('#post-id').value;
+    const comment = document.querySelector('input[name="post-id"]').value
+    const body = document.querySelector('textarea[name="comment-body"]').value
+
+    if (comment && post_id) {
         const response = await fetch(`/api/posts/${post_id}/comment`, {
-          method: 'POST',
-          body: JSON.stringify({ body }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
-    
+            
+            method: 'POST',
+            body: JSON.stringify({ body, comment }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });console.log(response)
+
         if (response.ok) {
-          document.location.reload();
+            document.location.reload();
         } else {
-          alert('Failed to create project');
+            alert('Failed to create project');
         }
-      }
-  
-  
-    
-  };
+    }
+
+};
+document
+  .querySelector('.media')
+  .addEventListener('submit', addCommentHandler);
