a31724c2fa514495a2ef2134d06ec91aca2e80b6
conradjohnson <skaterjames@gmail.com>
2022-10-18 10:58:34 -0500

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
-<<<<<<< HEAD
-const beerRoutes = require('./beerRoutes');
-
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-router.use('/beer', beerRoutes);
-=======
 const beerlistRoutes = require('./beerlistRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/beerlist', beerlistRoutes);
->>>>>>> 8d5e5c35bed8a800d3768c4255d6a242c429d59c
 
 module.exports = router;
